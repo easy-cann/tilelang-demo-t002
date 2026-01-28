ded52f39d6e9738e86ecee340a114a96340d856d
erhsh <caojian5@huawei.com>
2026-01-28 17:29:15 +0800

    adapt shmem

--- compile_run.sh
+++ compile_run.sh
@@ -13,7 +13,10 @@ bisheng \
     -I${ASCEND_HOME_PATH}/pkg_inc/profiling \
     -I${TL_ROOT}/src \
     -I${TL_ROOT}/3rdparty/catlass/include \
+    -I${TL_ROOT}/3rdparty/shmem/include \
+    -I${TL_ROOT}/3rdparty/shmem/src/device \
     -L${ASCEND_HOME_PATH}/lib64 \
+    -Wno-macro-redefined -Wno-ignored-attributes -Wno-non-c-typedef-for-linkage \
     -lruntime -lascendcl -lm -ltiling_api -lplatform -lc_sec -ldl \
     -fPIC --shared ${SCRIPT_DIR}/test_flash_attention.cpp -o ${SCRIPT_DIR}/test_flash_attention.so
 
